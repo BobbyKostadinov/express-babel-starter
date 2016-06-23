ccf8dc84732e53e5eba1a94872f173992214bdc8
Bobby Kostadinov <Bobby.Kostadinov@dowjones.com>
2016-06-23 11:26:38 +0100

    fixes and missing packages

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require('babel-core/register');
 var port = process.env.PORT || 3000;
 var app = require('./server/app');
-console.log(app.listen);
 app.listen(port, function () {
-  console.log('RoboWars app listening on port ' + port);
+  console.log('Express app listening on port ' + port);
 });
